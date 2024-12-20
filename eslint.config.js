88d63aa76f62232052c7834fc0f9d2766f747e66
satooru65536 <ryuto4619@gmail.com>
2024-12-21 02:24:04 +0900

    feat: Cloudflare対応のための依存関係の更新と新しいエントリポイントの追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config';
 
 export default antfu({
-  ignores: ['*.md', '*.json'],
+  ignores: ['*.md', '*.json', 'functions/[[path]].ts'],
   formatters: true,
   stylistic: {
     quotes: 'single',
